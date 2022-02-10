e98c916d6d528025f0ff806dfb55165f048f6bf1
4litasci <47966406+4litasci@users.noreply.github.com>
2022-02-11 00:00:40 +0300

    env updated

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,7 @@ import ActorsRoute from '@routes/actors.route';
 import MoviesRoute from '@routes/movies.route';
 import SocialRoute from '@routes/social.auth.route';
 
-validateEnv();
+//validateEnv();
 
 const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new ActorsRoute(), new MoviesRoute(), new SocialRoute()]);
 
